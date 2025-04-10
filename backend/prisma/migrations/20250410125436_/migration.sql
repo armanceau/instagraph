fc0707d62d602a46201ca32fc36e590fe2bb5797
Arthur Manceau <102350268+armanceau@users.noreply.github.com>
2025-04-10 14:58:58 +0200

    feat(): Ajout de champ sur l'utilisateur

--- backend/prisma/migrations/20250410125436_/migration.sql
+++ backend/prisma/migrations/20250410125436_/migration.sql
@@ -2,7 +2,9 @@
 CREATE TABLE "User" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "username" TEXT NOT NULL,
-    "password" TEXT NOT NULL
+    "password" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "ntel" TEXT NOT NULL
 );
 
 -- CreateTable
--- backend/src/domain/user/mutations.ts
+++ backend/src/domain/user/mutations.ts
@@ -1,61 +1,77 @@
 import { GraphQLError } from "graphql";
-import { comparePasswords, createJWT, hashPassword } from "../../modules/auth.js";
+import {
+  comparePasswords,
+  createJWT,
+  hashPassword,
+} from "../../modules/auth.js";
 import { ModelMutationResolvers } from "../../types.js";
 import { WithRequired } from "../../utils/mapped-types.js";
 
-type UserMutations = WithRequired<ModelMutationResolvers, 'createUser' | 'signIn'>
+type UserMutations = WithRequired<
+  ModelMutationResolvers,
+  "createUser" | "signIn"
+>;
 
-
-const createUser: NonNullable<ModelMutationResolvers['createUser']> = async (_, {username, password}, {dataSources: {db}}) => {
+const createUser: NonNullable<ModelMutationResolvers["createUser"]> = async (
+  _,
+  { username, password, email, ntel },
+  { dataSources: { db } }
+) => {
   try {
     const createdUser = await db.user.create({
       data: {
         username,
-        password: await hashPassword(password)
-      }
+        password: await hashPassword(password),
+        email,
+        ntel,
+      },
     });
-  
+
     return {
       code: 201,
-      message: 'user has been created',
+      message: "user has been created",
       success: true,
-      user: createdUser
-    }
+      user: createdUser,
+    };
   } catch {
     return {
       code: 400,
-      message: 'User has not been created',
+      message: "User has not been created",
       success: false,
       user: null,
-    }
+    };
   }
-}
+};
 
-const signIn: NonNullable<ModelMutationResolvers['signIn']> = async (_, {username, password}, {dataSources: {db}}) => {
+const signIn: NonNullable<ModelMutationResolvers["signIn"]> = async (
+  _,
+  { username, password },
+  { dataSources: { db } }
+) => {
   try {
-    const user = await db.user.findFirstOrThrow({where: {username}})
-    const isValidPassword = await comparePasswords(password, user.password)
-  
+    const user = await db.user.findFirstOrThrow({ where: { username } });
+    const isValidPassword = await comparePasswords(password, user.password);
+
     if (!isValidPassword) {
-      throw new GraphQLError('Something wrong happened')
+      throw new GraphQLError("Something wrong happened");
     }
-  
-    const token = createJWT(user)
-  
+
+    const token = createJWT(user);
+
     return {
       code: 200,
-      message: 'User authenticated',
+      message: "User authenticated",
       success: true,
       token,
-    }
+    };
   } catch (e) {
     return {
       code: 401,
       message: `${(e as Error)?.message}`,
       success: false,
       token: null,
-    }
+    };
   }
-}
+};
 
-export const userMutations: UserMutations = {createUser, signIn}
+export const userMutations: UserMutations = { createUser, signIn };
